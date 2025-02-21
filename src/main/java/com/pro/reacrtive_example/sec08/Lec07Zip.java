d1188efa467eed4f52657a41ddc90cc462d01dc2
priyanshu10 <priyanshuparate@gmail.com>
2025-02-21 14:06:44 +0530

    aggrigate zip merge

--- src/main/java/com/pro/reacrtive_example/sec08/Lec07Zip.java
+++ src/main/java/com/pro/reacrtive_example/sec08/Lec07Zip.java
@@ -1,4 +1,39 @@
 package com.pro.reacrtive_example.sec08;
 
+import com.pro.reacrtive_example.common.Util;
+import reactor.core.publisher.Flux;
+
+import java.awt.*;
+import java.time.Duration;
+
 public class Lec07Zip {
+
+    public static void main(String[] args) {
+
+        Flux.zip(getbody(),getEngine(),getTires())
+                .map(x->new Car(x.getT1(),x.getT2(),x.getT3()))
+                .subscribe(Util.subscriber());
+        Util.sleepSeconds(5);
+    }
+    private static Flux<String> getbody(){
+
+        return  Flux.range(1,5)
+                .map(x->"body-"+x)
+                .delayElements(Duration.ofMillis(100));
+    }
+
+    record  Car(String body, String engine, String tires){}
+    private static Flux<String> getEngine(){
+
+        return  Flux.range(1,3)
+                .map(x->"engine-"+x)
+                .delayElements(Duration.ofMillis(200));
+    }
+
+    private static Flux<String> getTires(){
+
+        return  Flux.range(1,10)
+                .map(x->"tires-"+x)
+                .delayElements(Duration.ofMillis(70));
+    }
 }
--- src/main/java/com/pro/reacrtive_example/sec08/assignment/ZipAssigment.java
+++ src/main/java/com/pro/reacrtive_example/sec08/assignment/ZipAssigment.java
@@ -1,2 +1,18 @@
-package com.pro.reacrtive_example.sec08.assignment;public class ZipAssigment {
+package com.pro.reacrtive_example.sec08.assignment;
+
+import com.pro.reacrtive_example.common.Util;
+
+public class ZipAssigment {
+
+    public static void main(String[] args) {
+        ProductHttpClient httpClient= new ProductHttpClient();
+
+        for(int i= 0;i<10;i++){
+            httpClient.getProduct(i)
+                    .subscribe(Util.subscriber());
+
+        }
+        Util.sleepSeconds(10);
+    }
 }
+
