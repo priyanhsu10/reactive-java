0b94c9952f1bc16af87cee45e759720e4b98663b
priyanshu10 <priyanshuparate@gmail.com>
2025-02-26 09:48:46 +0530

    handliing error

--- src/main/java/com/pro/reacrtive_example/sec08/Lec14collectList.java
+++ src/main/java/com/pro/reacrtive_example/sec08/Lec14collectList.java
@@ -12,10 +12,14 @@ public class Lec14collectList {
 
         var t= Flux.range(1, 10)
                 .concatWith(Flux.error(new RuntimeException("oops")))
+                .onErrorResume((e)->fallback())
                         .collectList();
 
 
-
+t.subscribe(Util.subscriber());
         Util.sleepSeconds(10);
+    }
+    public  static  Flux<Integer> fallback(){
+        return  Flux.just(1);
     }
 }
